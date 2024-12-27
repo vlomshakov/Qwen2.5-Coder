303e29f6306c8f0cd630ca31de06ef553a2ce34c
vadim.lomshakov <vadim.lomshakov@gmail.com>
2024-12-27 13:26:59 +0300

    ++

--- qwencoder-eval/instruct/livecode_bench/test.sh
+++ qwencoder-eval/instruct/livecode_bench/test.sh
@@ -3,7 +3,7 @@ OUTPUT_DIR="./output"
 echo "LiveCodeBench: ${MODEL_DIR}, OUPTUT_DIR: ${OUTPUT_DIR}"
 
 
-python -m lcb_runner.runner.main --model "tgi" --scenario codegeneration --evaluate --output_dir ${OUTPUT_DIR}
+python -m lcb_runner.runner.main --model "tgi" --multiprocess 4 --timeout 360 --scenario codegeneration --evaluate --output_dir ${OUTPUT_DIR}
 saved_eval_all_file="${OUTPUT_DIR}/log.json"
-python -m lcb_runner.evaluation.compute_scores --eval_all_file ${saved_eval_all_file} --start_date 2024-05-01
+python -m lcb_runner.evaluation.compute_scores --eval_all_file ${saved_eval_all_file} --start_date 2024-07-01
 
